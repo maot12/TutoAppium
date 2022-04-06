4e507fe1501d429be58ae12d5ec3181fe4eac681
miguel-angel.ortega <mortega2@ES-8049266.getronics.int-it.net>
2022-04-06 15:14:15 +0200

    Probando

---
+++ TutorialMaven/src/base.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class base {
+
+	public static AndroidDriver capabilities() throws MalformedURLException {
+		// TODO Auto-generated method stub
+				
+		File appDir = new File("src");
+		
+		File app = new File(appDir, "ApiDemos-debug.apk");
+		
+		DesiredCapabilities cap = new DesiredCapabilities();
+		
+		//Línea par declarar el nombre del emulador, cambiar si se requiere
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME,"PruebaAppium");
+		
+		cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+		
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+		
+		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+		
+		return driver;
+	
+	}
+
+}
---
+++ TutorialMaven/src/main.java
@@ -0,0 +1,14 @@
+import java.net.MalformedURLException;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class main extends base{
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		
+		AndroidDriver driver = capabilities();
+		
+	}
+
+}
