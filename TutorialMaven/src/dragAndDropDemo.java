6245acaea059b658290a4dfae4ab7743a5f9fb1e
Miguel Ortega <miguel-angel.ortega@getronics.com>
2022-04-19 10:20:40 +0200

    Drag and drope sin errores

---
+++ TutorialMaven/src/dragAndDropDemo.java
@@ -0,0 +1,52 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+import static java.time.Duration.ofSeconds;
+
+public class dragAndDropDemo extends conectorAndroidDriver {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			AndroidDriver driver = capabilities();
+			
+			driver.findElement(By.xpath("//android.widget.TextView[@text='Views']")).click();
+			driver.findElement(By.xpath("//android.widget.TextView[@text='Drag and Drop']")).click();
+			
+			@SuppressWarnings({ "rawtypes" })
+			TouchAction touch = new TouchAction(driver);
+			
+			//Longpress/move/release
+			WebElement source = driver.findElement(By.xpath("//android.view.View[@index='1']"));
+			WebElement destin = driver.findElement(By.xpath("//android.view.View[@index='2']"));
+			
+			/*
+			 * touch.longPress(longPressOptions() .withElement(element(source)))
+			 * .moveTo(element(destin)) .release() .perform();
+			 */
+			
+			//Código más simplificado			
+			touch.longPress(element(source))
+			.moveTo(element(destin))
+			.release()
+			.perform();
+			
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+		
+
+	}
+
+}
